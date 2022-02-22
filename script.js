62c4ef9967f2d2693daaa6f2a79934fb9bf4fec3
r3m0d <remod0147@gmail.com>
2022-02-22 12:14:06 +0100

    4es 5os template es javascript letrehozas

---
+++ script.js
@@ -0,0 +1,8 @@
+function ellenorzes(){
+    var pont = 0;
+    var negyedikmegoldas = "Fekete-erdő";
+    if(document.getElementById("legnagyobbmap").value == negyedikmegoldas){
+        pont +=1;
+    }
+    console.log(pont);
+}
